01404aba9505049b46ea7ded0379995519017e95
Joshua Zarazovski <joshuazarazovski@gmail.com>
2025-03-15 18:47:37 +1100

    Setup inheritance for templates - Setup config file

--- routes.py
+++ routes.py
@@ -5,4 +5,18 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Joshua'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'Joey'},
+            'body': 'Grand time in Melbourne'
+        },
+        {
+            'author': {'username': 'Bobby'},
+            'body': 'A closer look at HTMX'
+        },
+        {
+            'author': {'username': 'Lisa'},
+            'body': 'What\'s up with DevOps?'
+        }        
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
